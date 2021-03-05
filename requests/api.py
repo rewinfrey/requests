16c4dc96b6112fefaf40b822f4c23155008de850
Rick Winfrey <rewinfrey@github.com>
2021-03-05 13:48:33 -0800

    Use unsafe_code()

--- requests/api.py
+++ requests/api.py
@@ -12,12 +12,6 @@ This module implements the Requests API.
 
 from . import sessions
 
-from different_package import unsafe_code
-unsafe_code()
-
-from chardet import unsafe_code
-unsafe_code()
-
 def request(method, url, **kwargs):
     """Constructs and sends a :class:`Request <Request>`.
 

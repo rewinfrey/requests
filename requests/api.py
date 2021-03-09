f55e3b931f5e8874cee05da62616cbba8e1fe170
monalisa <octocat@github.com>
2021-03-09 12:33:02 -0800

    Update api.py

--- requests/api.py
+++ requests/api.py
@@ -12,9 +12,6 @@ This module implements the Requests API.
 
 from . import sessions
 
-import chardet
-chardet.unsafe_code()
-
 def request(method, url, **kwargs):
     """Constructs and sends a :class:`Request <Request>`.
 
